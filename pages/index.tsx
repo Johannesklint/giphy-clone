050239cd6d5ca3352b3446a1e1ade0707797d499
johkli <johannes.klint@etraveligroup.com>
2021-01-04 21:06:55 +0100

    Add loading spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,48 @@ import { gql } from 'graphql-request'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { Grid, GridItem } from '../components/grid'
 import LoginSignup from '../components/login-signup'
 import { useUser } from '../components/user'
 import { useGraphql } from './hooks/useGraphql'
+
+const rotate = keyframes`
+    0% {
+      transform: rotate(0);
+      animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+    }
+    50% {
+      transform: rotate(900deg);
+      animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+    }
+    100% {
+      transform: rotate(1800deg);
+  }
+`
+
+const Loader = styled.div`
+  display: flex;
+  margin: 20vh auto;
+  position: relative;
+  width: 80px;
+  height: 80px;
+
+  ::after {
+    content: ' ';
+    display: block;
+    border-radius: 50%;
+    width: 0;
+    height: 0;
+    margin: 8px;
+    box-sizing: border-box;
+    border: 32px solid #fff;
+    border-color: #fff transparent #fff transparent;
+    animation-name: ${rotate};
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+  }
+`
 
 const Item = styled.li`
   position: relative;
@@ -48,7 +85,7 @@ export default function Home() {
   )
 
   if (!data) {
-    return 'loading'
+    return <Loader />
   }
 
   return (
@@ -59,7 +96,7 @@ export default function Home() {
       </Head>
       {!user ? <LoginSignup /> : null}
       <ul>
-        <Grid gutter={10} columnWidth={250} rowHeight={20}>
+        <Grid gutter={10} columnWidth={200} rowHeight={2}>
           {data.getTrending.map(
             (gifs: {
               id: string
